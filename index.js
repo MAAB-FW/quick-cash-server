abc9621aaf5b2229b749c306498419a257d09b6a
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-07-16 18:08:37 +0600

    update: jwt related api and verify token modified with local storage

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cookieParser());
+// app.use(cookieParser());
 //Must remove "/" from your production URL
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173", "https://quickcash-job-task.vercel.app"],
         credentials: true,
     }),
 );
@@ -31,11 +31,6 @@ const client = new MongoClient(uri, {
     },
 });
 
-const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-};
 
 async function run() {
     try {
@@ -47,11 +42,14 @@ async function run() {
 
         // middleware
         const verifyToken = (req, res, next) => {
-            const token = req?.cookies?.token;
-            if (!token) return res.status(401).send({ message: "Unauthorized access!" });
+            // console.log("inside verify token", req.headers.authorization)
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "unauthorized access" });
+            }
+            const token = req.headers.authorization.split(" ")[1];
             jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
                 if (err) {
-                    return res.status(401).send({ message: "Unauthorized access!" });
+                    return res.status(401).send({ message: "unauthorized access" });
                 }
                 req.decoded = decoded;
                 next();
@@ -60,6 +58,7 @@ async function run() {
 
         app.get("/userInfo", verifyToken, async (req, res) => {
             user = req.decoded;
+            // console.log(user);
             const result = await usersCollection.findOne({ email: req.decoded.email });
             res.send(result);
         });
@@ -67,7 +66,7 @@ async function run() {
         app.post("/jwt", (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: "365d" });
-            res.cookie("token", token, cookieOptions).send({ success: true });
+            res.send({ token });
         });
 
         app.post("/logout", (req, res) => {
